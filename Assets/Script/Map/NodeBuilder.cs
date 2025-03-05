dc1bbb8a51ae03f7ba052258746c3e8effdb9210
copper09as <494699676@qq.com>
2025-03-05 17:50:28 +0800

    优化代码结构

---
+++ Assets/Script/Map/NodeBuilder.cs
@@ -0,0 +1,79 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class NodeBuilder
+{
+    bool collapsed;
+    Transform parent;
+    private float nodeRange;
+    private Vector2 NodesOffest;
+    List<MapNode> nodes;
+    public NodeBuilder(bool collaspsed,Transform parent,float nodeRange, Vector2 NodesOffestX,List<MapNode>nodes)
+    {
+        this.parent = parent;
+        this.collapsed = collaspsed;
+        this.nodeRange = nodeRange;
+        this.NodesOffest = NodesOffestX;
+        this.nodes = nodes;
+    }
+    public void CreateNode(int x,int y,NodeCreater creater)
+    {
+        float NodesOffestX = NodesOffest.x;
+        float NodesOffestY = NodesOffest.y;
+        Addressables.InstantiateAsync("MapNode").Completed += handle =>
+        {
+            
+            SpriteRenderer spriteRenderer = handle.Result.GetComponent<SpriteRenderer>();
+            MapNode node = handle.Result.GetComponent<MapNode>();
+            Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+            Vector2 position = new Vector2(x * spriteSize.x * nodeRange + NodesOffestX, -y * spriteSize.y * nodeRange + NodesOffestY);
+            handle.Result.transform.SetParent(parent, false);
+            handle.Result.transform.position = position;
+            node.transPos = new Vector2Int(x, y);
+            node.collapsed = collapsed;
+            node.gameObject.name = node.transPos.ToString();
+            node.creater = creater;
+            node.Init();
+        };
+    }
+    public void AddAdj(List<MapNode> nodes,MapNode node)
+    {
+        List<MapNode> tempNodes = nodes.Where(i => i != node && GetSqrDistance(node,i)<=2).ToList();
+        foreach(var adjNode in tempNodes)
+        {
+            node.AddAdj(adjNode);
+        }
+    }
+    public void RandomRemove(ref int RandomTimes,ref int times,List<MapNode> collapsedNodes)
+    {
+        int index = Random.Range(0, nodes.Count);
+        var node = nodes[index];
+
+        bool canRemove = true;
+        if (canRemove)
+        {
+            node.collapsed = true;
+            node.gameObject.SetActive(false);
+            nodes.RemoveAt(index);
+            collapsedNodes.Add(node);
+            times--;
+        }
+        else
+        {
+            RandomTimes += 1;
+        }
+
+    }
+    public float GetSqrDistance(MapNode node, MapNode otherNode)
+    {
+        float dx = node.transPos.x - otherNode.transPos.x;
+        float dy = node.transPos.y - otherNode.transPos.y;
+        return dx * dx + dy * dy;
+    }
+
+}
--- Assets/Script/Map/NodeCreater.cs
+++ Assets/Script/Map/NodeCreater.cs
@@ -1,18 +1,22 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class NodeCreater : MonoBehaviour
 {
-
-    [SerializeField] private List<MapNode> nodes;
-    [SerializeField] private List<MapNode> collapsedNodes;
     [SerializeField] private int DeleteCount;
+    [Header("是否开启环形修正")]
+    [SerializeField] private bool isRound;
+    [Header("当前存在的节点")]
+    public List<MapNode> nodes;
+    [Header("已坍缩的节点")]
+    [SerializeField] private List<MapNode> collapsedNodes;
     [Header("是否可出现传送城")]
     [SerializeField] private bool isMagicCity;
+    [Header("传送城判定参数")]
+    [SerializeField] private float magicCityDis;
     [Header("是否可出现孤岛")]
     [SerializeField] private bool isLonely;
     [Header("节点最大连线数")]
@@ -38,113 +42,59 @@ public class NodeCreater : MonoBehaviour
     [SerializeField] private float nodeRange;
     private Dictionary<int, int> collapsedXnode = new Dictionary<int, int>();
     private Dictionary<int, int> collapsedYnode = new Dictionary<int, int>();
-    private int collapsedXnodeCount;
-    private int collapsedYnodeCount;
-
+    private NodeBuilder nodeBuilder;
     IEnumerator Start()
     {
+        nodeBuilder = new NodeBuilder(false, transform, nodeRange, new Vector2(NodesOffestX, NodesOffestY),nodes);
         Random.InitState(MapSeed);
         yield return CreateNodeFirst(); // 等待节点初始化完成
         StartCoroutine(RandomNodeFirst(DeleteCount));
     }
     private IEnumerator CreateNodeFirst()
     {
-        List<AsyncOperationHandle<GameObject>> handles = new List<AsyncOperationHandle<GameObject>>();
         for (int i = 0; i < NodeWidth; i++)
         {
             for (int j = 0; j < NodeHeight; j++)
             {
-                int x = i;
-                int y = j;
-
-                Addressables.InstantiateAsync("MapNode").Completed += handle =>
-                {
-                    handle.Result.transform.SetParent(transform, false);
-                    SpriteRenderer spriteRenderer = handle.Result.GetComponent<SpriteRenderer>();
-                    MapNode node = handle.Result.GetComponent<MapNode>();
-                    Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
-                    Vector2 position = new Vector2(x * spriteSize.x * nodeRange + NodesOffestX, -y * spriteSize.y * nodeRange + NodesOffestY);
-                    handle.Result.transform.position = position;
-                    node.transPos = new Vector2Int(x, y);
-                    node.collapsed = false;
-                    node.gameObject.name = node.transPos.ToString();
-                    handles.Add(handle);
-                    nodes.Add(node);
-                };
+                nodeBuilder.CreateNode(i, j, this);
             }
         }
         yield return new WaitUntil(() => nodes.Count == NodeWidth * NodeHeight);
     }
-    private void CreateNodeSecond()
+    private void AddAdjNode()
     {
-        for (int i = 0; i < nodes.Count; i++)
+
+
+        foreach(var node in nodes)
         {
-            var node = nodes[i];
-            for (int j = i + 1; j < nodes.Count; j++)
-            {
-                var node2 = nodes[j];
-
-                // 计算平方距离
-                float dx = node.transPos.x - node2.transPos.x;
-                float dy = node.transPos.y - node2.transPos.y;
-                float sqrDistance = dx * dx + dy * dy;
-
-                // 检查是否相邻
-                if (sqrDistance <= 2 && i != j)
-                {
-                    node.adjancentNode.Add(node2);
-                    node2.adjancentNode.Add(node);
-                }
-
-            }
+            nodeBuilder.AddAdj(nodes, node);
         }
+    }
+    public float GetSqrDistance(MapNode node, MapNode otherNode)
+    {
+        float dx = node.transPos.x - otherNode.transPos.x;
+        float dy = node.transPos.y - otherNode.transPos.y;
+        return dx * dx + dy * dy;
     }
     private IEnumerator RandomNodeFirst(int times)
     {
         int RandomTimes = 0;
         int initTimes = times;
+        yield return null;
         while (times > 0)
         {
-            yield return null;
+            
             int index = Random.Range(0, nodes.Count);
             var node = nodes[index];
             int keyX = node.transPos.x;
             int keyY = node.transPos.y;
             bool canRemove = true;
-            DetectDic(collapsedXnode, NodeWidth, ref canRemove, keyX);
-            DetectDic(collapsedYnode, NodeHeight, ref canRemove, keyY);
-            /*if (collapsedXnode.ContainsKey(keyX))
+            if(isRound)
             {
-                if (collapsedXnode[keyX] >= NodeWidth - 1)
-                {
-                    canRemove = false;
-                    Debug.Log("X无法形成连通图");
-                }
-                else
-                {
-                    collapsedXnode[keyX] += 1;
-                }
+                DetectDic(collapsedXnode, NodeWidth, ref canRemove, keyX);
+                DetectDic(collapsedYnode, NodeHeight, ref canRemove, keyY);
             }
-            else
-            {
-                collapsedXnode.Add(keyX, 1);
-            }
-            if (collapsedYnode.ContainsKey(keyY))
-            {
-                if (collapsedYnode[keyY] >= NodeHeight - 1)
-                {
-                    canRemove = false;
-                    Debug.Log("Y无法形成连通图");
-                }
-                else
-                {
-                    collapsedYnode[keyY] += 1;
-                }
-            }
-            else
-            {
-                collapsedYnode.Add(keyY, 1);
-            }*/
+            //nodeBuilder.RandomRemove(RandomTimes);
             if (canRemove)
             {
                 node.collapsed = true;
@@ -159,12 +109,12 @@ public class NodeCreater : MonoBehaviour
             }
             if (RandomTimes > initTimes * 3)
             {
-                Debug.LogError("循环次数过多");
+                Debug.LogWarning("循环次数过多");
+                break;
             }
-            Debug.Log(index);
 
         }
-        CreateNodeSecond();
+        AddAdjNode();
         RandomNodeSecond();
         RandomNodeThird();
     }
@@ -172,12 +122,10 @@ public class NodeCreater : MonoBehaviour
     {
         List<MapNode> nodesToRemove = new List<MapNode>();
 
-        // 遍历 nodes 列表
         for (int i = 0; i < nodes.Count; i++)
         {
             var node = nodes[i];
 
-            // 检查相邻节点数量
             if (node.adjancentNode.Count >= maxAdj || node.adjancentNode.Count <= minAdj)
             {
                 foreach (var adjNode in node.adjancentNode)
@@ -186,25 +134,19 @@ public class NodeCreater : MonoBehaviour
                 }
                 node.collapsed = true;
                 node.gameObject.SetActive(false);
-                nodesToRemove.Add(node); // 将节点添加到待移除列表
+                nodesToRemove.Add(node);
                 collapsedNodes.Add(node);
             }
         }
 
-        // 移除需要折叠的节点
         foreach (var node in nodesToRemove)
         {
             nodes.Remove(node);
         }
-        /*foreach(var node in collapsedNodes)
-        {
-            Destroy(node.gameObject);
-        }*/
 
     }
-    private void RandomNodeThird()
+    private void MapNodeOffset()
     {
-        int index = 0;
         foreach (var node in nodes)
         {
             // 生成随机偏移量
@@ -217,6 +159,9 @@ public class NodeCreater : MonoBehaviour
             node.transform.position.y + randomOffsetY
         );
         }
+    }
+    private void HelpLonelyNode()
+    {
         for (int i = 0; i < nodes.Count; i++)
         {
 
@@ -228,9 +173,11 @@ public class NodeCreater : MonoBehaviour
                     connectNode.adjancentNode.Add(nodes[i]);
                     nodes[i].adjancentNode.Add(connectNode);
                 }
-
             }
         }
+    }
+    private void DeleteFriendNode()
+    {
         for (int i = 0; i < nodes.Count; i++)
         {
             if (nodes[i].adjancentNode.Count >= maxAdjNode)
@@ -241,52 +188,38 @@ public class NodeCreater : MonoBehaviour
                     nodes[i].adjancentNode[randomRemoveAdj].adjancentNode.Remove(nodes[i]);
                     nodes[i].adjancentNode.RemoveAt(randomRemoveAdj);
                 }
-
-            }
-
-            if (nodes[i].adjancentNode.Count >= 3)
-            {
-                index = i;
             }
         }
-        foreach (var node in nodes)
-        {
-            if (!isLonely)
-            {
-                
-                int addIndext = nodes.Count - 1;
-                while (!IsRemovalSafe(node.transPos) &&addIndext>0)
-                {
-                    addIndext--;
-                    Debug.Log(addIndext+"addIndex");
-                    if (!node.adjancentNode.Contains(nodes[addIndext]) && node != nodes[addIndext])
-                    {
-                        if(isMagicCity)
-                        {
-                            node.adjancentNode.Add(nodes[addIndext]);
-                            nodes[addIndext].adjancentNode.Add(node);
-                            continue;
-                        }
-                            
-                        if (!(Mathf.Abs(node.transPos.x - nodes[addIndext].transPos.x) + Mathf.Abs(node.transPos.y - nodes[addIndext].transPos.y) > Mathf.Max(NodeHeight,NodeWidth)/2))
-                        {
-                            node.adjancentNode.Add(nodes[addIndext]);
-                            nodes[addIndext].adjancentNode.Add(node);
-                            continue;
-                        }
-                        
-                    }  
-                }
-            }
-        }
+    }
+    private void RandomNodeThird()
+    {
+        MapNodeOffset();
+        HelpLonelyNode();
+        DeleteFriendNode();
+        LonelyCityDec();
         foreach (var node in nodes)
         {
             node.DrawLine();
         }
-
-        MapManager.Instance.TransPlace(nodes[index]);
-
-
+        MapManager.Instance.TransPlace(nodes[0]);
+    }
+    private void LonelyCityDec()
+    {
+        if (isLonely) return;
+        foreach (var node in nodes)
+        {
+            int addIndext = nodes.Count - 1;
+            var nodeRange = GetSqrDistance(node, nodes[addIndext]);
+            while (!IsRemovalSafe(node.transPos) && addIndext > 0)
+            {
+                addIndext--;
+                if (isMagicCity || (nodeRange <= (NodeHeight*NodeHeight+NodeWidth*NodeWidth)/magicCityDis))
+                {
+                    node.AddAdj(nodes[addIndext]);
+                    continue;
+                }
+            }
+        }
     }
     private void DetectDic(Dictionary<int, int> posDic, int range, ref bool canRemove, int key)
     {
@@ -331,9 +264,9 @@ public class NodeCreater : MonoBehaviour
                 }
             }
         }
-        Debug.LogWarning((visited.Count >= (nodes.Count) / 2) + position.ToString());
+        Debug.LogWarning((visited.Count > (nodes.Count) / 2) + position.ToString());
 
-        return visited.Count >= (nodes.Count) / 2;
+        return visited.Count > (nodes.Count) / 2;
     }
     private List<Vector2Int> GetNeighbors(Vector2Int position)
     {
