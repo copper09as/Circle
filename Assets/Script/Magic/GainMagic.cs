53c28bdd473bffbc7a8ae1eabb41ff081d881f53
lafuluode <2506199818@qq.com>
2025-03-13 19:46:11 +0800

    更改获取魔法

--- Assets/Script/Magic/GainMagic.cs
+++ Assets/Script/Magic/GainMagic.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
-public class GainMagic : MonoBehaviour
+public class GainMagic 
 {
-    public static void Gain(string name)
+    public static async Task<Magic> Gain(string name)
     {
-        Addressables.LoadAssetAsync<Magic>(name).Completed += (handle)
-        =>
-        {
-            
-        };
+        var handle =Addressables.LoadAssetAsync<Magic>(name);
+        await handle.Task; 
+        return handle.Result;
     }
 }
